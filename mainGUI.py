9222425313fbde959e36404032904482a94c07b3
EnriqueThomaz <enriquethomaz18@gmail.com>
2021-08-17 16:08:13 -0300

    update GUI

--- mainGUI.py
+++ mainGUI.py
@@ -1,5 +1,7 @@
 import tkinter as Tk
 from tkinter.filedialog import askopenfilename
+
+from matplotlib.pyplot import text
 import estatistica
 import ajuste
 
@@ -8,10 +10,17 @@ class MeuApp(Tk.Frame):
         super().__init__(master)
         #master.geometry('250x100')
         master.resizable(False, False)
-        master.title('HistoMaker')
+        master.title('Stats')
 
         self.pack()        
         self.create_widgets()
+
+    def myHelp(self):
+        textoAjuda = "Este é o texto de ajuda!\nSelecione o arquivo .txt que você deseja usar no programa utilizando o botão 'Procurar'\n\nEste arquivo deve seguir o seguinte formato:\nvalor1\nvalor2\n...\nvalorx\n\n"
+        textoAjuda += "Se precisar adiconar incertezas, utilize ';' para separá-las de seu valor correspondente. Da seguinte forma: valor1;incertezaValor1\n\nNo caso de construção de retas de ajuste, onde se faz necessário "
+        textoAjuda += "a presença dos valores x e y, coloque primeiro todos os valores de x e suas incertezas e imediatamente após os valores de y da seguinte forma:\nvalor_x1;incerteza_x1\nvalor_x2;incerteza_x2\nvalor_xk;incerteza_xk\n"
+        textoAjuda += "valor_y1;incerteza_y1\nvalor_y2;incerteza_y2\nvalor_yk;incerteza_yk\n\nVale ressaltar que os números devem ser expressos com '.' em vez de ','. Portanto o valor 1,234 está errado, o correto seria 1.234\n\nBom uso do programa! =D"
+        self.output.insert('end', textoAjuda)
 
     def browseFunc(self, destin):
         filename = askopenfilename(filetypes=(("text file", "*.txt"), ("All files", "*.*"),))
@@ -26,28 +35,31 @@ class MeuApp(Tk.Frame):
         
         respAjuste = ajuste.execute(path, options)
 
+        textoResposta = ''
+
         #Configurar pra abrir uma nova tela e mostrar os resultados
         if options["numDados"]:
-            print(respEsta["numDados"])
+            textoResposta += "Número de dados = " + str(respEsta["numDados"]) + "\n"
         if options["media"]:
-            print(respEsta["media"])
+            textoResposta += "Média = " + str(respEsta["media"]) + "\n"
         if options["moda"]:
-            print(respEsta["moda"])
+            textoResposta += "Moda = " + str(respEsta["moda"]) + "\n"
         if options["mediana"]:
-            print(respEsta["mediana"])
+            textoResposta += "Mediana = " + str(respEsta["mediana"]) + "\n"
         if options["dp"]:
-            print(respEsta["dp"])
+            textoResposta += "Desvio-Padrão = " + str(respEsta["dp"]) + "\n"
         if options["dpM"]:
-            print(respEsta["dpM"])
+            textoResposta += "Desvio-Padrão da média = " + str(respEsta["dpM"]) + "\n"
         if options["min"]:
-            print(respEsta["min"])
+            textoResposta += "Valor mínimo = " + str(respEsta["min"]) + "\n"
         if options["max"]:
-            print(respEsta["max"])
+            textoResposta += "Valor máximo = " + str(respEsta["max"]) + "\n"
         if options["freq"]:
-            print(respEsta["freq"])
+            textoResposta += "\nFrequências: " + "\n" + str(respEsta["freq"]) + "\n"
         if options["ajuste"]:
-            print(respAjuste)
-
+            textoResposta += "Coeficiente Angular = " + str(respAjuste[0]) + " ± " + str(respAjuste[2]) + "\n" + "Coeficiente linear = " + str(respAjuste[1]) + " ± " + str(respAjuste[3])
+        
+        self.output.insert('end', textoResposta)
 
     def create_widgets(self):
         self.titleLabel = Tk.Label(self, text="Selecione o arquivo de dados")
@@ -55,21 +67,30 @@ class MeuApp(Tk.Frame):
         self.locationLabel = Tk.Label(self, text="Local: ")
         self.escolhasLabel = Tk.Label(self, text='Marque o que desejas calcular:')
 
-        self.titleLabel.grid(row=0, column=1, columnspan=2, sticky="W")
+        self.titleLabel.grid(row=0, column=1, sticky="N")
         self.locationLabel.grid(row=1, column=0, sticky="E")
         self.escolhasLabel.grid(row=2, column=0, sticky="W")
 
         #Local path
         self.path = Tk.Entry(self)
 
-        self.path.grid(row=1, column=1)
+        self.path.grid(row=1, column=1, padx=20, sticky="NESW")
 
         #Buttons
         self.search = Tk.Button(self, text='Procurar', command=lambda: self.browseFunc(self.path))
         self.construct = Tk.Button(self, text="Calcular!", command=lambda: self.constructFunc(self.path.get()))
 
         self.search.grid(row=1, column=2, sticky="W")
-        self.construct.grid(row=8, column=1)
+        self.construct.grid(row=8, column=1, sticky="N")
+
+        self.helpBtn = Tk.Button(self, text="ajuda?", command=self.myHelp)
+        self.helpBtn.grid(row=8, column=2, sticky="E", padx=10, pady=10)
+
+        self.output = Tk.Text(self, height=10, padx=10, pady=10)
+        self.output.grid(row=9, column=0, columnspan=3, padx=20, pady=20)
+
+        self.credits = Tk.Label(self, text="©EnriqueThomaz / contato: enriquetd@id.uff.br")
+        self.credits.grid(row=10, column=0, sticky="W", columnspan=2)
 
         self.createCheckBoxes(3)
     
